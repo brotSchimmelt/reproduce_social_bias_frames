27d06b4008e0fea7002ada5353775d018b94889a
brotSchimmelt <tknebler+git@googlemail.com>
2024-02-05 23:22:13 +0100

    add tokens and sample template

--- config.py
+++ config.py
@@ -1,4 +1,42 @@
+# data paths
 SBIC_PATH = "data/SBIC/"
 SBIC_TEST_PATH = f"{SBIC_PATH}SBIC.v2.tst.csv"
 SBIC_TRAIN_PATH = f"{SBIC_PATH}SBIC.v2.trn.csv"
 SBIC_DEV_PATH = f"{SBIC_PATH}SBIC.v2.dev.csv"
+LOG_DIR = "logs/"
+
+# logging configuration
+IC_ENABLE = True
+LOGGING_LEVEL = "INFO"
+LOGGING_FORMAT = "%(asctime)s-%(levelname)s: %(message)s"
+LOGGING_DATEFMT = "%d-%m-%Y %H:%M:%S"
+LOGGING_FILE_DATEFMT = "%d_%m-%H_%M_%S"
+
+# special tokens
+START_TOKEN = "[STR]"
+END_TOKEN = "[END]"
+SEP_TOKEN = "[SEP]"
+OFF_TOKEN = {0: "[offN]", 1: "[offY]"}
+LEWD_TOKEN = {
+    0: "[lewdN]",
+    1: "[lewdY]",
+}
+INT_TOKEN = {
+    0: "[intN]",
+    1: "[intY]",
+}
+GRP_TOKEN = {
+    0: "[grpN]",
+    1: "[grpY]",
+}
+ING_TOKEN = {
+    0: "[ingN]",
+    1: "[ingY]",
+}
+
+# sample template
+SAMPLE_TEMPLATE = (
+    f"{START_TOKEN} {{post}} {SEP_TOKEN} {{lewd}} {{off}} "
+    f"{{intention}} {{grp}} {SEP_TOKEN} "
+    f"{{group}} {SEP_TOKEN} {{statement}} {SEP_TOKEN} {{ing}} {END_TOKEN}"
+)
