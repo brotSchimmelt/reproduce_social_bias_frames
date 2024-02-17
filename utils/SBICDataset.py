b08d36202aa947113a01d9ea7d9226c597147edf
brotSchimmelt <tknebler+git@googlemail.com>
2024-02-17 18:50:32 +0100

    add different templates based on variable assignments

--- utils/SBICDataset.py
+++ utils/SBICDataset.py
@@ -1,3 +1,5 @@
+import logging
+
 import pandas as pd
 from torch.utils.data import Dataset
 from transformers import GPT2Tokenizer
@@ -12,6 +14,8 @@ class SBICDataset(Dataset):
         self.path = path
         self.df = pd.read_csv(self.path)
         self.generation_prompts, self.targets = [], []
+        self.pad_value = tokenizer.pad_token_id
+        self.max_length = config.MAX_LENGTH
 
         # iterate over the dataframe and prepare the data
         for _, row in self.df.iterrows():
@@ -27,37 +31,66 @@ class SBICDataset(Dataset):
 
             # create samples
             generation_prompt = config.GENERATION_TEMPLATE.format(post=post).strip()
-            target = config.TARGET_TEMPLATE.format(
-                lewd=config.LEWD_TOKEN[lewd],
-                off=config.OFF_TOKEN[off],
-                intention=config.INT_TOKEN[intention],
-                grp=config.GRP_TOKEN[grp],
-                group=group,
-                statement=statement,
-                ing=config.ING_TOKEN[ing],
-            ).strip()
+
+            # format target based on possible variable assignments
+            if off == 0 and lewd == 0:
+                target = config.OFFN_TARGET_TEMPLATE.format(
+                    lewd=config.LEWD_TOKEN[lewd],
+                    off=config.OFF_TOKEN[off],
+                ).strip()
+            elif off == 1 and grp == 0:
+                target = config.OFFY_GRPN_TARGET_TEMPLATE.format(
+                    lewd=config.LEWD_TOKEN[lewd],
+                    off=config.OFF_TOKEN[off],
+                    intention=config.INT_TOKEN[intention],
+                    grp=config.GRP_TOKEN[grp],
+                ).strip()
+            else:
+                target = config.FULL_TARGET_TEMPLATE.format(
+                    lewd=config.LEWD_TOKEN[lewd],
+                    off=config.OFF_TOKEN[off],
+                    intention=config.INT_TOKEN[intention],
+                    grp=config.GRP_TOKEN[grp],
+                    group=group,
+                    statement=statement,
+                    ing=config.ING_TOKEN[ing],
+                ).strip()
 
             self.generation_prompts.append(generation_prompt)
             self.targets.append(target)
+
+        # remove filler tokens
+        if not config.USE_FILL_TOKEN:
+            self.generation_prompts = [
+                p.replace(config.FILL_TOKEN, "") for p in self.generation_prompts
+            ]
+            self.targets = [t.replace(config.FILL_TOKEN, "") for t in self.targets]
 
         # tokenize the data
         self.encoded_prompts = tokenizer(
             self.generation_prompts,
             truncation=True,
-            padding=True,
+            padding="max_length",  # NOTE change from True to 'max_length'
             max_length=config.MAX_LENGTH,
             return_tensors="pt",
         )
         self.encoded_targets = tokenizer(
             self.targets,
             truncation=True,
-            padding=True,
+            padding="max_length",  # NOTE change from True to 'max_length'
             max_length=config.MAX_LENGTH,
             return_tensors="pt",
         )
         self.input_ids = self.encoded_prompts["input_ids"]
         self.attention_mask = self.encoded_prompts["attention_mask"]
         self.labels = self.encoded_targets["input_ids"]
+
+        logging.info(
+            f"len(Input_ids): {len(self.input_ids)} | len(labels): {len(self.labels)}"
+        )
+        logging.info(f"Input_id lengths: {set([len(i) for i in self.input_ids])}")
+        logging.info(f"Label lengths: {set([len(i) for i in self.labels])}")
+        logging.info(f"Use FILL token: {config.USE_FILL_TOKEN}")
 
     def __len__(self) -> int:
         return len(self.labels)
