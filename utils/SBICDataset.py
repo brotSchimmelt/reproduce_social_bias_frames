843e0d1829161d25f60212f7ad4fecf24f2cab40
brotSchimmelt <tknebler+git@googlemail.com>
2024-02-26 10:32:50 +0100

    fix label for text generation

--- utils/SBICDataset.py
+++ utils/SBICDataset.py
@@ -1,6 +1,8 @@
 import logging
+from typing import Dict
 
 import pandas as pd
+import torch
 from torch.utils.data import Dataset
 from transformers import GPT2Tokenizer
 
@@ -9,16 +11,11 @@ from utils.helper import clean_post
 
 
 class SBICDataset(Dataset):
-    def __init__(
-        self, path: str, tokenizer: GPT2Tokenizer, fill_token_flag: bool
-    ) -> None:
+    def __init__(self, path: str, tokenizer: GPT2Tokenizer) -> None:
         self.tokenizer = tokenizer
         self.path = path
-        self.fill_token_flag = fill_token_flag
         self.df = pd.read_csv(self.path)
-        self.generation_prompts, self.targets = [], []
-        self.pad_value = tokenizer.pad_token_id
-        self.max_length = config.MAX_LENGTH
+        self.texts = []
 
         # iterate over the dataframe and prepare the data
         for _, row in self.df.iterrows():
@@ -27,88 +24,74 @@ class SBICDataset(Dataset):
             lewd = 1 if row["sexYN"] == 1.0 else 0
             off = 1 if row["offensiveYN"] == 1.0 else 0
             intention = 1 if row["intentYN"] == 1.0 else 0
-            grp = 1 if row["targetMinority"] else 0
+            grp = 1 if row["whoTarget"] == 1.0 else 0
             ing = 1 if row["speakerMinorityYN"] == 1.0 else 0
-            group = row["targetMinority"]
-            statement = row["targetStereotype"]
+            group = (
+                row["targetMinority"] if isinstance(row["targetMinority"], str) else ""
+            )
+            statement = (
+                row["targetStereotype"]
+                if isinstance(row["targetStereotype"], str)
+                else ""
+            )
 
-            # create samples
-            generation_prompt = config.GENERATION_TEMPLATE.format(post=post).strip()
-
-            # format target based on possible variable assignments
+            # create samples based on possible variable assignments
             if off == 0 and lewd == 0:
-                target = config.OFFN_TARGET_TEMPLATE.format(
-                    lewd=config.LEWD_TOKEN[lewd],
-                    off=config.OFF_TOKEN[off],
-                ).strip()
+                self.texts.append(
+                    config.TRAIN_TEMPLATE_OFFN.format(
+                        post=post,
+                        lewd=config.LEWD_TOKEN[lewd],
+                        off=config.OFF_TOKEN[off],
+                    ).strip()
+                )
+
             elif off == 1 and grp == 0:
-                target = config.OFFY_GRPN_TARGET_TEMPLATE.format(
-                    lewd=config.LEWD_TOKEN[lewd],
-                    off=config.OFF_TOKEN[off],
-                    intention=config.INT_TOKEN[intention],
-                    grp=config.GRP_TOKEN[grp],
-                ).strip()
+                self.texts.append(
+                    config.TRAIN_TEMPLATE_GRPN.format(
+                        post=post,
+                        lewd=config.LEWD_TOKEN[lewd],
+                        off=config.OFF_TOKEN[off],
+                        intention=config.INT_TOKEN[intention],
+                        grp=config.GRP_TOKEN[grp],
+                    ).strip()
+                )
+
             else:
-                target = config.FULL_TARGET_TEMPLATE.format(
-                    lewd=config.LEWD_TOKEN[lewd],
-                    off=config.OFF_TOKEN[off],
-                    intention=config.INT_TOKEN[intention],
-                    grp=config.GRP_TOKEN[grp],
-                    group=group,
-                    statement=statement,
-                    ing=config.ING_TOKEN[ing],
-                ).strip()
-
-            self.generation_prompts.append(generation_prompt)
-            self.targets.append(target)
-
-        # remove filler tokens
-        if not self.fill_token_flag:
-            self.generation_prompts = [
-                p.replace(config.FILL_TOKEN, "").strip()
-                for p in self.generation_prompts
-            ]
-            self.targets = [t.replace(config.FILL_TOKEN, "") for t in self.targets]
+                self.texts.append(
+                    config.TRAIN_TEMPLATE_FULL.format(
+                        post=post,
+                        lewd=config.LEWD_TOKEN[lewd],
+                        off=config.OFF_TOKEN[off],
+                        intention=config.INT_TOKEN[intention],
+                        grp=config.GRP_TOKEN[grp],
+                        group=group,
+                        statement=statement,
+                        ing=config.ING_TOKEN[ing],
+                    ).strip()
+                )
 
         # tokenize the data
-        self.encoded_prompts = tokenizer(
-            self.generation_prompts,
+        self.encoding_dict = tokenizer(
+            self.texts,
             truncation=True,
-            padding="max_length",  # NOTE change from True to 'max_length'
             max_length=config.MAX_LENGTH,
+            padding="max_length",
             return_tensors="pt",
         )
-        self.encoded_targets = tokenizer(
-            self.targets,
-            truncation=True,
-            padding="max_length",  # NOTE change from True to 'max_length'
-            max_length=config.MAX_LENGTH,
-            return_tensors="pt",
-        )
-        self.input_ids = self.encoded_prompts["input_ids"]
-        self.attention_mask = self.encoded_prompts["attention_mask"]
-        self.labels = self.encoded_targets["input_ids"]
-
-        # log the lengths of the input_ids and labels
-        logging.info(
-            f"len(Input_ids): {len(self.input_ids)} | len(labels): {len(self.labels)}"
-        )
-        logging.info(
-            f"Input_id lengths: {set([len(i) for i in self.input_ids])} | max_length: {self.max_length}"
-        )
-        logging.info(
-            f"Label lengths: {set([len(i) for i in self.labels])} | max_length: {self.max_length}"
-        )
-        logging.info(f"Use FILL token: {self.fill_token_flag} | {config.FILL_TOKEN}")
-        logging.info(f"First Generation Prompt: {self.generation_prompts[0]}")
-        logging.info(f"First Target: {self.targets[0]}")
+        self.input_ids = self.encoding_dict["input_ids"]
+        self.attention_mask = self.encoding_dict["attention_mask"]
+        logging.info(f"First text: {self.texts[0]}")
 
     def __len__(self) -> int:
-        return len(self.labels)
+        return len(self.input_ids)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Dict[str, torch.Tensor]:
+        input_ids = self.input_ids[idx]
+        attention_mask = self.attention_mask[idx]
+        labels = input_ids.clone()
+
         return {
-            "input_ids": self.input_ids[idx],
-            "attention_mask": self.attention_mask[idx],
-            "labels": self.labels[idx],
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": labels,
         }
