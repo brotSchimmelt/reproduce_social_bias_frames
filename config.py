82dc7bf6de1c850729d0c3fcbdb0449a12d5da6e
brotSchimmelt <tknebler+git@googlemail.com>
2024-02-27 07:22:05 +0100

    add path to evaluate prompts

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ SBIC_PATH = f"{DATA_PATH}SBIC/"
 SBIC_TEST_PATH = f"{SBIC_PATH}SBIC.v2.tst.csv"
 SBIC_TRAIN_PATH = f"{SBIC_PATH}SBIC.v2.trn.csv"
 SBIC_DEV_PATH = f"{SBIC_PATH}SBIC.v2.dev.csv"
+DEV_EVAL_PROMPTS = f"{DATA_PATH}dev_eval_prompts.csv"
+TEST_EVAL_PROMPTS = f"{DATA_PATH}test_eval_prompts.csv"
 MODEL_PATH = "tmp/models/"
 OUTPUT_PATH = "tmp/output/"
 LOG_DIR = "logs/"
@@ -18,26 +20,25 @@ CHECKPOINT_DIR = "tmp/checkpoints/"
 # model paths
 GPT2_SMALL = "/bigwork/nhwpknet/models/gpt2-small"
 GPT2_XL = "/bigwork/nhwpknet/models/gpt2-xl"
-DEFAULT_MODEL = GPT2_SMALL  # version of GPT2 not mentioned in paper
+DEFAULT_MODEL = GPT2_XL  # version of GPT2 not mentioned in paper
 
 # random seeds
 DEFAULT_SEED = 42
-EXPERIMENT_SEEDS = [42, 1337, 31415, 271828, 1701]
+EXPERIMENT_SEEDS = [42]  # 1337, 31415, 271828, 1701
 
 # model settings
-MAX_LENGTH = 256  # not mentioned in paper
+MAX_LENGTH = 128  # not mentioned in paper
 LOGGING_STEPS = 500
 DEFAULT_WARMUP_STEPS = 500  # paper did mention warum up, but not the number of steps
 DEFAULT_BATCH_SIZE = 4  # from paper: 4
 DEFAULT_LEARNING_RATE = 1e-5  # from paper: 1e-5
 DEFAULT_NUM_EPOCHS = 5  # from paper: 1,2,5
-DEFAULT_NUM_RETURN_SEQ = 10  # from paper for sampling based inference: 10
 
 # inference settings
-INFERENCE_BATCH_SIZE_GREEDY = 512
-INFERENCE_BATCH_SIZE_SAMPLING = 64
+INFERENCE_BATCH_SIZE_GREEDY = 512  # max for 256 token model
+INFERENCE_BATCH_SIZE_SAMPLING = 64  # max for 256 token model
 PADDING_SIDE = "left"
-NUM_RETURN_SEQ = 10
+NUM_RETURN_SEQ = 10  # from paper for sampling based inference: 10
 
 # special tokens
 START_TOKEN = "<|startoftext|>"
